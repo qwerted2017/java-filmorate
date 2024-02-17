e79c90ec481d4c6381b866b46856b3f08f37cc7c
lukaval <forumchanko@gmail.com>
2024-02-17 23:46:34 +0300

    checkstyle fix 1

--- src/main/java/ru/yandex/practicum/filmorate/annotations/MinimumDateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/annotations/MinimumDateValidator.java
@@ -5,14 +5,16 @@ import javax.validation.ConstraintValidatorContext;
 import java.time.LocalDate;
 
 public class MinimumDateValidator implements ConstraintValidator<MinimumDate, LocalDate> {
+
     private LocalDate minimumDate;
+
     @Override
-    public void initialize(MinimumDate constraintAnnotation){
+    public void initialize(MinimumDate constraintAnnotation) {
         minimumDate = LocalDate.parse(constraintAnnotation.value());
     }
 
     @Override
-    public boolean isValid(LocalDate value, ConstraintValidatorContext context){
+    public boolean isValid(LocalDate value, ConstraintValidatorContext context) {
         return value == null || !value.isBefore(minimumDate);
     }
 }
