e48843147b8792aef09186f235a65f911e049ad2
lukaval <forumchanko@gmail.com>
2024-03-03 16:54:23 +0300

    fix review comments and github checkstyle errors #3

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -5,9 +5,14 @@ import ru.yandex.practicum.filmorate.models.Film;
 import java.util.List;
 
 public interface FilmStorage {
+
     List<Film> getFilms();
+
     Film getFilmById(int id);
+
     Film addFilm(Film film);
+
     void deleteFilm(Film film);
+
     Film updateFilm(Film film);
 }
