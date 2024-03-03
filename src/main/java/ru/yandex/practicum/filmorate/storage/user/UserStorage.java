2ed798b2361ec416924b74e8f0dba6eacddd2e2a
lukaval <forumchanko@gmail.com>
2024-03-03 16:55:41 +0300

    fix review comments and github checkstyle errors #4

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -4,7 +4,9 @@ import ru.yandex.practicum.filmorate.models.User;
 import java.util.List;
 
 public interface UserStorage {
+
     List<User> getUsers();
+
     User getUserById(int userId);
 
     User addUser(User user);
