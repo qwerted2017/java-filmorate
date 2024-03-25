794dc7048fdd73cfe250db3e94f9db6d29fab624
lukaval <forumchanko@gmail.com>
2024-03-25 23:20:22 +0300

    first commit (second start is success)

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO "genre" ("name")
+VALUES ( 'Комедия' ), ('Драма'), ('Мультфильм'), ('Триллер'), ('Документальный'), ('Боевик');
+
+INSERT INTO "mpa_rating" ("name")
+VALUES ( 'G' ),('PG'),('PG-13'),('R'),('NC-17');
